6e93898e6264bd91ebb10b82413866af276a44aa
PainOfExistance <matej113a@gmail.com>
2023-08-17 19:56:04 +0200

    modified:   .vscode/settings.json 	modified:   asset_loader.py 	modified:   game.py 	new file:   map.txt

--- asset_loader.py
+++ asset_loader.py
@@ -26,12 +26,12 @@ class AssetLoader:
         img_rect.center = center
         return img, img_rect
     
-    def load_player(self, path):
+    def load_player(self, path, center):
         # Load player image
         player = pygame.image.load(path)
         player = pygame.transform.scale(player, (self.screen_width // 10, self.screen_height // 10))
         player_rect = player.get_rect()
-        player_rect.center = (self.screen_width // 2, self.screen_height // 2)
+        player_rect.center = center
         return player, player_rect
     
     def load_background(self, path):
@@ -52,6 +52,7 @@ class AssetLoader:
         image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         binary_image = 1 - (image // 255)
         print(binary_image)
+        #np.savetxt('map.txt', binary_image, fmt='%d')
         return binary_image
     
     #https://www.youtube.com/watch?v=vOn0z0IRVN8&list=PLI2unizewPmmLdFX9kTGPSnXJJCiasCw5&index=64&ab_channel=Nazareth-Topic
--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+import math
 import pygame
 import sys
 import numpy as np
 from asset_loader import AssetLoader
+from scipy.stats import linregress
 
 class Game:
     def __init__(self):
@@ -10,18 +12,19 @@ class Game:
 
         # Set up display
         self.screen_width, self.screen_height = 800, 600
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height))
         pygame.display.set_caption("Finding Habo")
 
         # Load player image
-        assets=AssetLoader(self.screen_width, self.screen_height)
+        asets = AssetLoader(self.screen_width, self.screen_height)
 
-        self.player, self.player_rect = assets.load_player("desk1.png")
-        self.background, self.bg_rect = assets.load_background("bg.png")
-        #self.image, self.image_rect = assets.load_images("desk3.png", (100, 100), (250, 250))
+        self.player, self.player_rect = asets.load_player("desk1.png", (self.screen_width // 2, self.screen_height // 2))
+        self.background, self.bg_rect = asets.load_background("bg.png")
+        # self.image, self.image_rect = asets.load_images("desk3.png", (100, 100), (250, 250))
 
         # Create a collision map using NumPy
-        self.collision_map =assets.load_collision("bg.png")
+        self.collision_map = asets.load_collision("bg.png")
 
         # Set up clock for controlling frame rate
         self.clock = pygame.time.Clock()
@@ -44,58 +47,57 @@ class Game:
                 pygame.quit()
                 sys.exit()
 
-    def check_collision(self, player_rect, movement):
-        # Calculate the new player's position after movement
-        new_player_rect = player_rect.copy()
-        new_player_rect.move_ip(movement)
-
-        # Check for collision with obstacles
-        relative_player_left = int(new_player_rect.left - self.bg_rect.left)
-        relative_player_right = int(new_player_rect.right - self.bg_rect.left)
-        relative_player_top = int(new_player_rect.top - self.bg_rect.top)
-        relative_player_bottom = int(new_player_rect.bottom - self.bg_rect.top)
-
-        if np.count_nonzero(self.collision_map[relative_player_top:relative_player_bottom, relative_player_left:relative_player_right] == 1) > 0:
-            return 1  # Collision with obstacle
-        else:
-            return 0  # No collision
-
     def update(self):
         # Calculate delta time (time since last frame)
         current_time = pygame.time.get_ticks()
-        self.delta_time = (current_time - self.last_frame_time) / 1000.0  # Convert to seconds
+        self.delta_time = (current_time - self.last_frame_time) / \
+            1000.0  # Convert to seconds
         self.last_frame_time = current_time
-
+        
+        relative_player_left = int(self.player_rect.left - self.bg_rect.left)
+        relative_player_right = int(self.player_rect.right - self.bg_rect.left)
+        relative_player_top = int(self.player_rect.top - self.bg_rect.top)
+        relative_player_bottom = int(self.player_rect.bottom - self.bg_rect.top)
         movement = int(self.movement_speed * self.delta_time)
 
+        print(f"rl: {relative_player_left},   rr: {relative_player_right},   rt: {relative_player_top},   rb: {relative_player_bottom}")
+
         keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a] and not self.check_collision(self.player_rect, (-movement, 0)):
+        if keys[pygame.K_a] and np.count_nonzero(self.collision_map[relative_player_top:relative_player_bottom, relative_player_left-movement] == 1) <= 1:
             if self.player_rect.left > 10:
-                self.player_rect.move_ip(-movement, 0)
+                self.player_rect.move_ip(
+                    int(-self.movement_speed * self.delta_time), 0)
             else:
-                self.bg_rect.move_ip(movement, 0)
+                self.bg_rect.move_ip(
+                    int(self.movement_speed * self.delta_time), 0)
 
-        if keys[pygame.K_d] and not self.check_collision(self.player_rect, (movement, 0)):
+        if keys[pygame.K_d] and np.count_nonzero(self.collision_map[relative_player_top:relative_player_bottom, relative_player_right+movement] == 1) <= 1:
             if self.player_rect.right < self.screen_width-10:
-                self.player_rect.move_ip(movement, 0)
+                self.player_rect.move_ip(
+                    int(self.movement_speed * self.delta_time), 0)
             else:
-                self.bg_rect.move_ip(-movement, 0)
-                
-        if keys[pygame.K_w] and not self.check_collision(self.player_rect, (0, -movement)):
-            if self.player_rect.top > 10:
-                self.player_rect.move_ip(0, -movement)
-            else:
-                self.bg_rect.move_ip(0, movement)
+                self.bg_rect.move_ip(
+                    int(-self.movement_speed * self.delta_time), 0)
 
-        if keys[pygame.K_s] and not self.check_collision(self.player_rect, (0, movement)):
-            if self.player_rect.bottom < self.screen_height-10:
-                self.player_rect.move_ip(0, movement)
+        if keys[pygame.K_w] and np.count_nonzero(self.collision_map[relative_player_top-movement, relative_player_left:relative_player_right] == 1) <= 1:
+            if self.player_rect.top > 10:
+                self.player_rect.move_ip(
+                    0, int(-self.movement_speed * self.delta_time))
             else:
-                self.bg_rect.move_ip(0, -movement)
+                self.bg_rect.move_ip(
+                    0, int(self.movement_speed * self.delta_time))
+
+        if keys[pygame.K_s] and np.count_nonzero(self.collision_map[relative_player_bottom+movement, relative_player_left:relative_player_right] == 1) <= 1:
+            if self.player_rect.bottom < self.screen_height-10:
+                self.player_rect.move_ip(
+                    0, int(self.movement_speed * self.delta_time))
+            else:
+                self.bg_rect.move_ip(
+                    0, int(-self.movement_speed * self.delta_time))
 
     def draw(self):
         self.screen.fill((230, 60, 20))
         self.screen.blit(self.background, self.bg_rect.topleft)
         self.screen.blit(self.player, self.player_rect.topleft)
-        #self.screen.blit(self.image, self.image_rect.topleft)
+        # self.screen.blit(self.image, self.image_rect.topleft)
+        pygame.display.flip()
