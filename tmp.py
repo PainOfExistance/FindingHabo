18b152f24e0cc4e005af6b7fe89851e5bb3e626c
PainOfExistance <matej113a@gmail.com>
2024-02-01 21:50:30 +0100

    started to implement le parser

--- tmp.py
+++ tmp.py
@@ -1,28 +1,131 @@
+import os
+import random
+
 from asset_loader import AssetLoader
 
-assets=AssetLoader()
+assets = AssetLoader()
+world = assets.load_level_data()
+items = assets.load_items()
+ai = assets.load_ai_package()
 
-world=assets.load_level_data()
+spawn = (0, 0)
+portals = []
+enemies = []
+final_items = []
+containers = []
+metadata = []
 
-entities = {
-    "Player_spawn": [],
-    "Enemy_Random_Spawn": [],
-    "Item_field": [],
-    "Container_field": [],
-    "Metadata": []
-}
 
-for entity_type, entity_list in world["entities"].items():
-    for entity in entity_list:
-        if entity_type == "Player_spawn":
-            entities["Player_spawn"].append(entity)
-        elif entity_type == "Enemy_Random_Spawn":
-            entities["Enemy_Random_Spawn"].append(entity)
-        elif entity_type == "Item_field":
-            entities["Item_field"].append(entity)
-        elif entity_type == "Container_field":
-            entities["Container_field"].append(entity)
-        elif entity_type == "Metadata":
-            entities["Metadata"].append(entity)
+def setEnemies(enemy):
+    for x in ai:
+        if (
+            ai[x]["stats"]["group"] == enemy["enemy_Type"]
+            and enemy["chance"] >= random.random()
+            and enemy["rarity"] == ai[x]["stats"]["rarity"]
+        ):
+            return x
 
-print(entities["Player_spawn"])
+
+def setItems(item):
+    if (
+        item["name"][0] == "common"
+        or item["name"][0] == "uncommon"
+        or item["name"][0] == "rare"
+        or item["name"][0] == "epic"
+        or item["name"][0] == "legendary"
+    ):
+        for x in items:
+            if (
+                items[x]["type"] == item["type"][0]
+                and item["chance"][0] >= random.random()
+                and item["name"][0] == items[x]["rarity"]
+            ):
+                return x
+    else:
+        for x in items:
+            if item["chance"][0] >= random.random() and item["name"][0] == x:
+                return x
+
+
+def setContainer(item):
+    tmp = []
+    for i, x in enumerate(item["items"]):
+        if (
+            x == "common"
+            or x == "uncommon"
+            or x == "rare"
+            or x == "epic"
+            or x == "legendary"
+        ):
+            for y in items:
+                if (
+                    items[y]["type"] == item["type"][i]
+                    and item["chance"][i] >= random.random()
+                    and item["items"][i] == items[y]["rarity"]
+                ):
+                    tmp.append(y)
+        else:
+            for y in items:
+                if (
+                    item["chance"][i] >= random.random()
+                    and item["items"][i] == y
+                    ):
+                        tmp.append(y)
+
+    return tmp
+
+
+for x in world["entities"]:
+    if x == "Player_spawn":
+        for y in world["entities"][x]:
+            if y["customFields"]["type"] == "default":
+                spawn = (y["x"], y["y"])
+            else:
+                portals.append((y["customFields"], y["x"], y["y"]))
+
+    elif x == "Enemy_Random_Spawn":
+        for y in world["entities"][x]:
+            tmp = setEnemies(y["customFields"])
+            if tmp:
+                enemies.append((tmp, y["x"], y["y"]))
+
+    elif x == "Item_field":
+        for y in world["entities"][x]:
+            tmp = setItems(y["customFields"])
+            if tmp:
+                final_items.append((tmp, y["x"], y["y"]))
+
+    elif x == "Container_field":
+        for y in world["entities"][x]:
+            tmp = setContainer(y["customFields"])
+            file_name=os.path.basename(y["customFields"]["image"]).split('/')[-1]
+            
+            if tmp:
+                containers.append(
+                    (
+                        tmp,
+                        y["x"],
+                        y["y"],
+                        y["customFields"]["name"],
+                        "textures/static/"+file_name,
+                    )
+                )
+                
+    elif x=="Metadata":
+        for i, _ in enumerate(world["entities"][x][0]["customFields"]["music"]):
+            world["entities"][x][0]["customFields"]["music"][i]=world["entities"][x][0]["customFields"]["music"][i][3:]       
+        metadata=world["entities"][x][0]["customFields"]
+
+print("-------------------")
+print(spawn)
+print("-------------------")
+print(portals)
+print("-------------------")
+print(enemies)
+print("-------------------")
+print(final_items)
+print("-------------------")
+print(containers)
+print("-------------------")
+print(metadata)
+print("-------------------")
