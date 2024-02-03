eb1f37bda9f6a8f09c77acc0096a4aac61d78e66
MatejPC <matej113a@gmail.com>
2024-02-03 16:31:06 +0100

    timedating

--- tmp.py
+++ tmp.py
@@ -1,132 +1,47 @@
-import os
-import random
-
-from asset_loader import AssetLoader
-
-assets = AssetLoader()
-world = assets.load_level_data()
-items = assets.load_items()
-ai = assets.load_ai_package()
-
-spawn = (0, 0)
-portals = []
-enemies = []
-final_items = []
-containers = []
-metadata = []
+import time
+from datetime import datetime, timedelta
 
 
-def setEnemies(enemy):
-    for x in ai:
-        if (
-            ai[x]["stats"]["group"] == enemy["enemy_Type"]
-            and enemy["chance"] >= random.random()
-            and enemy["rarity"] == ai[x]["stats"]["rarity"]
-        ):
-            return x
+class CustomDate:
+    def __init__(self):
+        # Define custom month names
+        self.custom_month_names = {
+            1: "Shining Dawn",  # January
+            2: "Dry Cut",  # February
+            3: "Earth Cultivation",  # March
+            4: "Small Grass",  # April
+            5: "Large Grass",  # May
+            6: "Flowers",  # June
+            7: "Small Sickle",  # July
+            8: "Large Sickle",  # August
+            9: "Nodding Fruit",  # September
+            10: "Wine Flowing",  # October
+            11: "Falling Leaves",  # November
+            12: "Biting Cold",  # December
+        }
+        # Set the starting date
+        self.start_date = datetime(1945, 5, 9)
+        self.start_date = self.start_date.replace(hour=9, minute=42, second=36)
+        self.current_date=self.start_date
+
+    def increment_seconds(self):
+        # Calculate the elapsed time since the start date
+        # elapsed_time = time.time() - self.start_date.timestamp()
+        # Add the elapsed time to the start date
+        self.current_date = self.current_date + timedelta(seconds=1)
+
+    def print_date(self):
+        # Replace the month number with the custom month name
+        custom_month_name = self.custom_month_names[self.current_date.month]
+        # Print the date with the custom month name and year
+        print(f"{self.current_date.day}.{custom_month_name}.{self.current_date.year}   {self.current_date.hour}:{self.current_date.minute}:{self.current_date.second}")
 
 
-def setItems(item):
-    if (
-        item["name"][0] == "common"
-        or item["name"][0] == "uncommon"
-        or item["name"][0] == "rare"
-        or item["name"][0] == "epic"
-        or item["name"][0] == "legendary"
-    ):
-        for x in items:
-            if (
-                items[x]["type"] == item["type"][0]
-                and item["chance"][0] >= random.random()
-                and item["name"][0] == items[x]["rarity"]
-            ):
-                return x
-    else:
-        for x in items:
-            if item["chance"][0] >= random.random() and item["name"][0] == x:
-                return x
+# Create an instance of the class
+my_date = CustomDate()
 
+# Increment seconds
+my_date.increment_seconds()
 
-def setContainer(item):
-    tmp = []
-    for i, x in enumerate(item["items"]):
-        if (
-            x == "common"
-            or x == "uncommon"
-            or x == "rare"
-            or x == "epic"
-            or x == "legendary"
-        ):
-            for y in items:
-                if (
-                    items[y]["type"] == item["type"][i]
-                    and item["chance"][i] >= random.random()
-                    and item["items"][i] == items[y]["rarity"]
-                ):
-                    tmp.append(y)
-        else:
-            for y in items:
-                if (
-                    item["chance"][i] >= random.random()
-                    and item["items"][i] == y
-                    ):
-                        tmp.append(y)
-
-    return tmp
-
-
-for x in world["entities"]:
-    if x == "Player_spawn":
-        for y in world["entities"][x]:
-            if y["customFields"]["type"] == "default":
-                spawn = (y["x"], y["y"])
-            else:
-                portals.append((y["customFields"], y["x"], y["y"]))
-
-    elif x == "Enemy_Random_Spawn":
-        for y in world["entities"][x]:
-            tmp = setEnemies(y["customFields"])
-            if tmp:
-                enemies.append((tmp, y["x"], y["y"]))
-
-    elif x == "Item_field":
-        for y in world["entities"][x]:
-            tmp = setItems(y["customFields"])
-            if tmp:
-                final_items.append((tmp, y["x"], y["y"]))
-
-    elif x == "Container_field":
-        for y in world["entities"][x]:
-            tmp = setContainer(y["customFields"])
-            file_name=os.path.basename(y["customFields"]["image"]).split('/')[-1]
-            containers.append(
-                    (
-                        tmp,
-                        y["x"],
-                        y["y"],
-                        y["customFields"]["name"],
-                        "textures/static/"+file_name,
-                    )
-                )
-                
-    elif x=="Metadata":
-        for i, _ in enumerate(world["entities"][x][0]["customFields"]["music"]):
-            world["entities"][x][0]["customFields"]["music"][i]=world["entities"][x][0]["customFields"]["music"][i][3:]       
-        metadata=world["entities"][x][0]["customFields"]
-
-print("-------------------")
-print(spawn)
-print("-------------------")
-print(portals)
-print("-------------------")
-print(enemies)
-print("-------------------")
-print(final_items)
-print("-------------------")
-print(containers)
-print("-------------------")
-print(metadata)
-print("-------------------")
-
-for x in containers:
-    print(x)
+# Print the date with the custom month name
+my_date.print_date()
