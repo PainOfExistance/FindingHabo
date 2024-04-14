afd3cc43c5412065b4c2fc793845d4a6d4e52eee
PainOfExistance <matej113a@gmail.com>
2024-04-14 21:10:29 +0200

    fixes

--- world_parser.py
+++ world_parser.py
@@ -106,9 +106,9 @@ def setActivators(activators):
         for i, x in enumerate(activators["script_runner"]):
             arr.append(json.loads(x))
         return {"script_runner": arr, "type": "script_runner", "ref": activators["action_ref"]}
-    elif activators["type"]!="":
+    elif activators["type"]=="smithing" or activators["type"]=="enchanting" or activators["type"]=="alchemy" or activators["type"]=="upgrade":
         return {"crafter": activators["type"], "type": "crafting", "ref": activators["action_ref"]}
-    else:
+    elif activators["type"]=="board":
         return {"type": "board", "ref": activators["action_ref"]}
 
 
